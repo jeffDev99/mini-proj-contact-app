265f51b21f90ed724d18118181a60d918e43506d
S.Mohammad.Amin.Jafarnezhad <mohammadaminjafarnezhad6@gmail.com>
2024-09-07 08:20:09 +0330

    make table responsive

--- src/Components/ContactsList/ContactsList.jsx
+++ src/Components/ContactsList/ContactsList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ContactRow from '../ContactRow/ContactRow.jsx'
+import ContactRow from "../ContactRow/ContactRow.jsx";
 import styles from "./ContactsList.module.css";
 
 export default function ContactsList({ contacts, setContacts, selectedContact, setSelectedContact, setContact, deleteHandler, setIsEditing }) {
@@ -11,28 +11,39 @@ export default function ContactsList({ contacts, setContacts, selectedContact, s
       <h4 className={styles.subtitle}>{contacts.length} TOTAL</h4>
       <h2 className={styles.title}>Contacts</h2>
       <input type="text" placeholder="Search by name or email" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className={styles.formSearch} style={{ marginBottom: "1rem" }} />
-      <table className={`${styles.table}`}>
-        <thead>
-          <tr>
-            <th>Full Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th style={{ width: "120px", alignItems: "center" }}>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredContacts.length ? (
-            filteredContacts.map((item) => <ContactRow setIsEditing={setIsEditing} deleteHandler={deleteHandler} setSelectedContact={setSelectedContact} selectedContact={selectedContact} setContact={setContact}  key={item.id} contact={item}  />)
-          ) : (
+      <div style={{overflowX:'auto'}}>
+        <table className={`${styles.table}`}>
+          <thead>
             <tr>
-              <td colSpan={6}>
-                <p className="alert alert-primary">No contact yet</p>
-              </td>
+              <th>Full Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th style={{ width: "120px", alignItems: "center" }}>Actions</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredContacts.length ? (
+              filteredContacts.map((item) => (
+                <ContactRow
+                  setIsEditing={setIsEditing}
+                  deleteHandler={deleteHandler}
+                  setSelectedContact={setSelectedContact}
+                  selectedContact={selectedContact}
+                  setContact={setContact}
+                  key={item.id}
+                  contact={item}
+                />
+              ))
+            ) : (
+              <tr>
+                <td colSpan={6}>
+                  <p className="alert alert-primary">No contact yet</p>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
-
